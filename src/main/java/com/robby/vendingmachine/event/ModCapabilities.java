9bf2941595fb06c2a10a0d44d0ccd206dd4e2ee2
gatiger <ivafireman@gmail.com>
2026-05-11 09:43:09 -0400

    adding top block interface to work with inventory input

--- src/main/java/com/robby/vendingmachine/event/ModCapabilities.java
+++ src/main/java/com/robby/vendingmachine/event/ModCapabilities.java
@@ -1,11 +1,17 @@
 package com.robby.vendingmachine.event;
 
 import com.robby.vendingmachine.VendingMachineMod;
-import com.robby.vendingmachine.registry.ModBlockEntities;
+import com.robby.vendingmachine.block.VendingMachineBlock;
+import com.robby.vendingmachine.blockentity.VendingMachineBlockEntity;
+import com.robby.vendingmachine.registry.ModBlocks;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.items.IItemHandler;
 
 @EventBusSubscriber(
         modid = VendingMachineMod.MOD_ID,
@@ -14,10 +20,26 @@ import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 public class ModCapabilities {
     @SubscribeEvent
     public static void registerCapabilities(RegisterCapabilitiesEvent event) {
-        event.registerBlockEntity(
+        event.registerBlock(
                 Capabilities.ItemHandler.BLOCK,
-                ModBlockEntities.VENDING_MACHINE.get(),
-                (blockEntity, side) -> blockEntity.getAutomationItemHandler()
+                (level, pos, state, blockEntity, side) -> {
+                    if (!(state.getBlock() instanceof VendingMachineBlock)) {
+                        return null;
+                    }
+
+                    BlockPos lowerPos = state.getValue(VendingMachineBlock.HALF) == DoubleBlockHalf.UPPER
+                            ? pos.below()
+                            : pos;
+
+                    BlockEntity lowerBlockEntity = level.getBlockEntity(lowerPos);
+
+                    if (lowerBlockEntity instanceof VendingMachineBlockEntity vendingMachineBlockEntity) {
+                        return vendingMachineBlockEntity.getAutomationItemHandler();
+                    }
+
+                    return null;
+                },
+                ModBlocks.VENDING_MACHINE.get()
         );
     }
 }
